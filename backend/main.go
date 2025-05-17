4e34694c842aa3459bb677203b45700b71bb20c6
kyyril <khairilrahman55@gmail.com>
2025-05-17 08:09:08 +0700

    feat: setup server

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,69 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
+}
+
+func main() {
+	// connect to the database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// create the users table
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//create router for crud with mux
+	router := mux.NewRouter()
+	router.HandleFunc("api/users", getUsers(db)).Methods("GET")
+	router.HandleFunc("api/users", createUser(db)).Methods("POST")
+	router.HandleFunc("api/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("api/users/{id}", updateUser(db)).Methods("PUT")
+	router.HandleFunc("api/users/{id}", deleteUser(db)).Methods("DELETE")
+
+	// wrap the router with CORS and JSON type middleware
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+	// start the server
+	log.Fatal(http.ListenAndServe(":8080", enhancedRouter))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		//set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// check if the request is for preflight
+		if r.Method == http.MethodOptions {
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
